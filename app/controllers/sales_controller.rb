5d184a5551324c47e5d8350ab6dff169595e315b
jmiguel14 <migueljurado74@gmail.com>
2025-04-04 00:11:10 -0500

    parse index ans show endpoints correctly for sales

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,12 +2,12 @@ class SalesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    sales = Sale.all.as_json(only: [ :id, :total_price, :created_at, :updated_at, :name, :description ])
+    sales = Sale.all.as_json(only: [ :id, :total_amount, :created_at, :updated_at, :name, :description ])
     render json: { sales: sales }, status: :ok
   end
 
   def show
     sale = Sale.find(params[:id])
-    render json: { sale: sale.as_json(include: [ :sale_details, seller: { only: [ :id, :name, :email, :roles ] } ]) }, status: :ok
+    render json: { sale: sale.as_json(include: [ sale_details: { include: [ :product ] }, seller: { only: [ :id, :name, :email, :roles ] } ]) }, status: :ok
   end
 end
