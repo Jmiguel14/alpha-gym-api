89bdbb27a2b2b7eeddb6e963566fd5d7c5d1684b
jmiguel14 <migueljurado74@gmail.com>
2025-05-03 23:08:06 -0500

    add secret key base

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
+# Require the gems listed in Gemfile, including any gems.
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
@@ -30,7 +30,7 @@ module AlphaGym
     config.api_only = true
 
     # Enabled the session store for api_only application
-    config.session_store :cookie_store, key: '_interslice_session'
+    config.session_store :cookie_store, key: "_interslice_session"
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
   end
