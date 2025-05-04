d0cd7519bdfdc2d2aec938481f71fcbcd139dca2
jmiguel14 <migueljurado74@gmail.com>
2025-05-03 23:23:43 -0500

    disable action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,9 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Disable Action Cable
+  config.action_cable.mount_path = nil
+  config.action_cable.url = nil
+  config.action_cable.allowed_request_origins = []
 end
